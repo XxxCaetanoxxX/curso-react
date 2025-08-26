8d4d08dd1812993080cab49fd11b682111abe794
caetano.cesar <caetano.cesar@defensoria.mg.def.br>
2025-08-26 09:53:59 -0300

    consumindo api json server

--- e-commerce/src/pages/home/index.tsx
+++ e-commerce/src/pages/home/index.tsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from "react"
 import { BsCartPlus } from "react-icons/bs"
 
+import { api } from '../../services/api'
+
+interface ProductProps {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    cover: string;
+}
+
 export function Home() {
+    const [products, setProducts] = useState<ProductProps[]>([])
+
+    useEffect(() => {
+        async function getProducts() {
+            const response = await api.get('/products');
+            setProducts(response.data);
+        }
+
+        getProducts();
+    }, [])
+
     return (
         <div>
             <main className="w-full max-w-7xl px-4 mx-auto ">
@@ -8,22 +30,27 @@ export function Home() {
 
                 <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
 
-                    <section className="w-full">
-                        
-                        <img
-                            className="w-full rounded-lg max-h-70 mb-2"
-                            alt="Logo do produto"
-                            src="https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-card-40-iphone16prohero-202409_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=MjUrdW9rK0I3Y0hBcFdUR2pNVTRtUFpIU2c1QXYxN1o5THJsVFdubi8vdU9STS9wYXZTN1hicnBjZ2p3Y1lQVndnZDdrU0tSek5hTHNwZzJKOWwvbnRrb3YwRE90eklmVkIwdHovcEFheWlka3BhTzl6cklqNm1lL21taEZoT3c" />
-                        <p className="font-medium mt-1 mb-2"> Iphone 14 pro max</p>
+                    {products.map((product) => (
+                        <section key={product.id} className="w-full">
 
-                        <div className="flex gap-3 items-center">
-                            <strong className="text-zinc-700/90">R$ 4.500,00</strong>
-                            <button className="bg-zinc-900 p-1 rounded">
-                                <BsCartPlus size={20} color="#FFF" />
-                            </button>
-                        </div>
+                            <img
+                                className="w-full rounded-lg max-h-70 mb-2"
+                                alt={product.title}
+                                src={product.cover} />
+                            <p className="font-medium mt-1 mb-2"> {product.title}</p>
 
-                    </section>
+                            <div className="flex gap-3 items-center">
+                                <strong className="text-zinc-700/90">{product.price.toLocaleString('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                })}</strong>
+                                <button className="bg-zinc-900 p-1 rounded">
+                                    <BsCartPlus size={20} color="#FFF" />
+                                </button>
+                            </div>
+
+                        </section>
+                    ))}
 
                 </div>
             </main>
